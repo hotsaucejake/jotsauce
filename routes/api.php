d118cdabf6ee9d6bdf94f0f78ac6f4eb32dcd66b
hotsaucejake <get@hotsaucejake.com>
2020-04-12 16:12:59 -0400

    passport wip

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function () {
+
+    Route::prefix('auth')->name('auth.')->namespace('Auth')->group(function () {
+        Route::post('login', 'AuthController@login')->name('login');
+        Route::post('register', 'AuthController@register')->name('register');
+      
+        Route::group(['middleware' => 'auth:api'], function() {
+            Route::get('logout', 'AuthController@logout')->name('logout');
+            Route::get('user', 'AuthController@user')->name('user');
+        });
+    });
+
 });
