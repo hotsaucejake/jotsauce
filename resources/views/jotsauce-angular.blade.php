15d193dab9497f030b2468c3173b4586f9123711
hotsaucejake <get@hotsaucejake.com>
2020-04-24 17:27:13 -0400

    create angular project

---
+++ resources/views/jotsauce-angular.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>JotsauceAngular</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="/assets/jotsauce-angular/styles.09e2c710755c8867a460.css"></head>
+<body>
+  <app-root></app-root>
+<script src="/assets/jotsauce-angular/runtime-es2015.66ee9f4ce05a5b0fae8b.js" type="module"></script><script src="/assets/jotsauce-angular/runtime-es5.66ee9f4ce05a5b0fae8b.js" nomodule defer></script><script src="/assets/jotsauce-angular/polyfills-es5.8e50a9832860f7cf804a.js" nomodule defer></script><script src="/assets/jotsauce-angular/polyfills-es2015.5b10b8fd823b6392f1fd.js" type="module"></script><script src="/assets/jotsauce-angular/main-es2015.6ea2be953b3274272b42.js" type="module"></script><script src="/assets/jotsauce-angular/main-es5.6ea2be953b3274272b42.js" nomodule defer></script></body>
+</html>
