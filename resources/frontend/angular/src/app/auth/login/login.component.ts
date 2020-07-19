58d5b35deb87a5efa1cbdcd001657d1e3dbbbe90
hotsaucejake <get@hotsaucejake.com>
2020-07-19 00:20:23 -0400

    close but not working, need to debug
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- resources/frontend/angular/src/app/auth/login/login.component.ts
+++ resources/frontend/angular/src/app/auth/login/login.component.ts
@@ -1,16 +1,30 @@
+import { AuthService } from './../../core/services/auth.service';
+import { UserCredentials } from './../../core/models/authentication/user-credentials.interface';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-auth-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [AuthService]
 })
 export class LoginComponent implements OnInit {
 
+  public componentIsLoading = false;
+
+  public userCredentials: UserCredentials = {
+    email: localStorage.getItem('email') ? localStorage.getItem('email') : '',
+    password: ''
+  };
+
+  public remember = true;
+
   constructor(
-    private readonly router: Router
+    private readonly authService: AuthService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
   ) { }
 
   public ngOnInit(): void {
@@ -20,8 +34,21 @@ export class LoginComponent implements OnInit {
 
   }
 
-  public login(): void {
-    throw new Error('not implemented');
+  public async login(): Promise<void> {
+    this.componentIsLoading = true;
+
+    if (!this.remember) {
+      localStorage.removeItem('email');
+    }
+
+    const response = await this.authService.login(this.userCredentials, this.remember);
+    if (response.type === 'data') {
+      console.log(response);
+    } else {
+      console.log(response);
+    }
+
+    this.componentIsLoading = false;
   }
 
 }
---
+++ resources/frontend/angular/src/app/core/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { BaseService } from "./_base.service";
+import { UserCredentials } from "../models/authentication/user-credentials.interface";
+import { ServiceResponse } from "./models/service-response.interface";
+import { AuthenticationResponse } from "../models/authentication/authentication-response.interface";
+import { AuthenticationService } from "../authentication/authentication.service";
+
+export class AuthService extends BaseService {
+
+    public async login(authModel: UserCredentials, remember: boolean): Promise<ServiceResponse<AuthenticationResponse>> {
+        const response = await this.postAsync<AuthenticationResponse>('auth/login', authModel);
+
+        if (response.type === 'data') {
+            AuthenticationService.setAuthentication(response.data, remember);
+
+            if (remember) {
+                localStorage.setItem('email', authModel.email);
+            }
+        }
+
+        return response;
+    }
+
+    public logout(): void {
+        AuthenticationService.clearAuthentication();
+        // TODO: redirect user to login here
+    }
+}
