c623839b4aaad45f1aac183e79a837ba85ffc29f
hotsaucejake <get@hotsaucejake.com>
2021-02-10 23:50:00 -0500

    switch frontend to jotsauce-angular external to this project

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class FrontendController extends Controller
 {
     public function index()
     {
-        return view('angular');
+        return view('jotsauce-angular');
     }
 }
